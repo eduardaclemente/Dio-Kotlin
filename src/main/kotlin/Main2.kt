1302076635da8b477799a85ae4dd44401cf11eed
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-08-19 17:54:53 -0300

    desafio Kotlin 2

---
+++ src/main/kotlin/Main2.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val ativos = mutableListOf<String>()
+
+    // Entrada da quantidade de ativos
+    val quantidadeAtivos = readLine()!!.toInt()
+
+    // Entrada dos códigos dos ativos
+    for (i in 1..quantidadeAtivos) {
+        val codigoAtivo = readLine()!!
+        ativos.add(codigoAtivo)
+    }
+
+    //TODO: Ordenar os ativos em ordem alfabética.
+    ativos.sort()
+    //TODO: Imprimir a lista de ativos ordenada, conforme a tabela de exemplos.
+    for (ativo in ativos) {
+        println(ativo)
+    }
+}
